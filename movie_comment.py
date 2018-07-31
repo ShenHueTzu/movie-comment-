39340c2f0bcde3d7e52fa06154cc86807b46433d
ShenHueTzu <40592822+ShenHueTzu@users.noreply.github.com>
2018-07-31 15:36:52 +0800

    Update movie_comment.py

--- movie_comment.py
+++ movie_comment.py
@@ -1,12 +1,13 @@
 
 # coding: utf-8
 
-# In[2]:
 
 
 import requests
 from bs4 import BeautifulSoup
 import lxml
+
+#get data from Yahoo movie with 柯南
 
 res = requests.get('https://movies.yahoo.com.tw/movieinfo_review.html/id=8102?sort=update_ts&order=desc&page=1')
 soup = BeautifulSoup(res.text,"lxml")
@@ -14,13 +15,10 @@ soup = BeautifulSoup(res.text,"lxml")
 title_inChinese = soup.find("h1",{"class":"inform_title"}).text
 title_inEng = soup.find("div",{"class":"inform_title_en"}).text
 
-
-# In[3]:
-
+#use selenium automatically manipulate webdriver(Chorme)
 
 from selenium import webdriver
 import time
-from bs4 import BeautifulSoup
 import re
 
 comment_total=[]
@@ -28,12 +26,12 @@ comment_total=[]
 browser = webdriver.Chrome()
 browser.get('https://movies.yahoo.com.tw/movieinfo_review.html/id=8102?sort=update_ts&order=desc&page=1')
 
-#page1to3
 
 for i in range(0,12):
     
-    time.sleep(20)
+    time.sleep(20) #avoid detecting 
     soup = BeautifulSoup(browser.page_source,"lxml")
+    
     
     comment = [tag.text for tag in soup.select('.form_good span')]
     comment = [el.replace('\n', '') for el in comment]
@@ -41,35 +39,27 @@ for i in range(0,12):
     
     comment_total.extend(comment)
     
+    #自動跳頁
     browser.find_element_by_css_selector('li.nexttxt > a').click()
 
 browser.close()     
 
 
-# In[4]:
-
-
+#過濾文字
 r=re.compile(" [0-9]|[0-9]")
 B= list(filter(r.match, comment_total))
 comment_total_new= [x for x in comment_total if x not in B]
 
-
-# In[14]:
-
-
+#convert list to a string
 mytext = ''.join(comment_total_new)
 
 
-# In[20]:
-
-
+#分詞
 import jieba 
 mytext = " ".join(jieba.cut(mytext))
 
 
-# In[25]:
-
-
+#
 from wordcloud import WordCloud 
 wordcloud = WordCloud(font_path="simsun.ttf").generate(mytext)
 
